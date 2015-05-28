38449b55adc04f1e20a273809f8645e74286d722
Thomas Jungblut <thjungbl@microsoft.com>
2015-05-28 16:20:15 +0100

    counting triangles

---
+++ src/de/jungblut/graph/search/TriangleCounter.java
@@ -0,0 +1,57 @@
+package de.jungblut.graph.search;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import de.jungblut.graph.Graph;
+import de.jungblut.graph.Tuple;
+import de.jungblut.graph.model.Vertex;
+
+public class TriangleCounter {
+
+  public static <K, V, E> int countTriangles(Graph<K, V, E> graph) {
+    int numTriangles = 0;
+
+    HashSet<K> visited = new HashSet<>();
+    for (K vId : graph.getVertexIDSet()) {
+
+      if (visited.add(vId)) {
+        for (Tuple<Vertex<K, V>, Vertex<K, V>> pair : pairs(graph, vId)) {
+
+          if (graph.getAdjacentVertices(pair.getFirst()).contains(
+              pair.getSecond())
+              && graph.getAdjacentVertices(pair.getSecond()).contains(
+                  pair.getFirst())) {
+
+            numTriangles++;
+            visited.add(pair.getFirst().getVertexId());
+            visited.add(pair.getSecond().getVertexId());
+          }
+        }
+      }
+    }
+
+    return numTriangles;
+  }
+
+  private static <K, V, E> List<Tuple<Vertex<K, V>, Vertex<K, V>>> pairs(
+      Graph<K, V, E> graph, K vertexId) {
+    List<Tuple<Vertex<K, V>, Vertex<K, V>>> lst = new ArrayList<>();
+
+    List<Vertex<K, V>> adjacents = ImmutableList.copyOf(graph
+        .getAdjacentVertices(vertexId));
+
+    for (int i = 0; i < adjacents.size(); i++) {
+      for (int j = i + 1; j < adjacents.size(); j++) {
+        lst.add(new Tuple<Vertex<K, V>, Vertex<K, V>>(adjacents.get(i),
+            adjacents.get(j)));
+      }
+    }
+
+    return lst;
+  }
+
+}
---
+++ test/de/jungblut/graph/search/TriangleCounterTest.java
@@ -0,0 +1,77 @@
+package de.jungblut.graph.search;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.jungblut.graph.AdjacencyList;
+import de.jungblut.graph.model.Edge;
+import de.jungblut.graph.model.VertexImpl;
+
+public class TriangleCounterTest {
+
+  @Test
+  public void testSimpleCase() {
+    AdjacencyList<Integer, Integer, Integer> g = new AdjacencyList<>();
+    g.addVertex(new VertexImpl<Integer, Integer>(1, 0));
+    g.addVertex(new VertexImpl<Integer, Integer>(2, 0));
+    g.addVertex(new VertexImpl<Integer, Integer>(3, 0));
+
+    g.addEdge(1, new Edge<Integer, Integer>(2, 0));
+    g.addEdge(1, new Edge<Integer, Integer>(3, 0));
+
+    g.addEdge(2, new Edge<Integer, Integer>(1, 0));
+    g.addEdge(2, new Edge<Integer, Integer>(3, 0));
+
+    g.addEdge(3, new Edge<Integer, Integer>(1, 0));
+    g.addEdge(3, new Edge<Integer, Integer>(2, 0));
+
+    int triangles = TriangleCounter.countTriangles(g);
+    Assert.assertEquals(1, triangles);
+  }
+
+  @Test
+  public void testBridge() {
+    AdjacencyList<Integer, Integer, Integer> g = new AdjacencyList<>();
+    g.addVertex(new VertexImpl<Integer, Integer>(1, 0));
+    g.addVertex(new VertexImpl<Integer, Integer>(2, 0));
+    g.addVertex(new VertexImpl<Integer, Integer>(3, 0));
+
+    g.addVertex(new VertexImpl<Integer, Integer>(4, 0));
+    g.addVertex(new VertexImpl<Integer, Integer>(5, 0));
+
+    g.addEdge(1, new Edge<Integer, Integer>(2, 0));
+    g.addEdge(1, new Edge<Integer, Integer>(3, 0));
+
+    g.addEdge(2, new Edge<Integer, Integer>(1, 0));
+    g.addEdge(2, new Edge<Integer, Integer>(3, 0));
+
+    g.addEdge(3, new Edge<Integer, Integer>(1, 0));
+    g.addEdge(3, new Edge<Integer, Integer>(2, 0));
+    g.addEdge(3, new Edge<Integer, Integer>(4, 0));
+    g.addEdge(3, new Edge<Integer, Integer>(5, 0));
+
+    g.addEdge(4, new Edge<Integer, Integer>(3, 0));
+    g.addEdge(4, new Edge<Integer, Integer>(5, 0));
+
+    g.addEdge(5, new Edge<Integer, Integer>(3, 0));
+    g.addEdge(5, new Edge<Integer, Integer>(4, 0));
+
+    int triangles = TriangleCounter.countTriangles(g);
+    Assert.assertEquals(2, triangles);
+  }
+
+  @Test
+  public void testNoTriangle() {
+    AdjacencyList<Integer, Integer, Integer> g = new AdjacencyList<>();
+    g.addVertex(new VertexImpl<Integer, Integer>(1, 0));
+    g.addVertex(new VertexImpl<Integer, Integer>(2, 0));
+    g.addVertex(new VertexImpl<Integer, Integer>(3, 0));
+
+    g.addEdge(1, new Edge<Integer, Integer>(2, 0));
+    g.addEdge(2, new Edge<Integer, Integer>(3, 0));
+
+    int triangles = TriangleCounter.countTriangles(g);
+    Assert.assertEquals(0, triangles);
+  }
+
+}
